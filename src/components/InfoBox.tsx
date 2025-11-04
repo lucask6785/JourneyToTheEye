2973f96f2663cd602955471971826a43647736df
lucask6785 <lucask6785@gmail.com>
2025-11-03 23:00:49 -0500

    Not found and distance display

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -6,7 +6,8 @@ export function InfoBox({
   pathCount, 
   selectedStar, 
   startingStar, 
-  destinationStar 
+  destinationStar,
+  pathDistance
 }: {
   totalStars: number;
   detailedCount: number;
@@ -14,9 +15,20 @@ export function InfoBox({
   selectedStar: StarData | null;
   startingStar: StarData | null;
   destinationStar: StarData | null;
+  pathDistance: number | null;
 }) {
   const formatStar = (star: StarData | null) => 
     star ? `${star.name} (ID: ${star.id})` : 'None';
+
+  const formatDistance = (distance: number | null) => {
+    if (!distance) {
+      return 'None';
+    } else if (distance == -1) {
+      return 'No Path Found';
+    } else {
+      return distance.toLocaleString() + ' parsecs';
+    }
+  }
 
   return (
     <div className="info-box">
@@ -26,6 +38,7 @@ export function InfoBox({
       <div className="info-item">Selected: {formatStar(selectedStar)}</div>
       <div className="info-item">Starting Star: {formatStar(startingStar)}</div>
       <div className="info-item">Destination Star: {formatStar(destinationStar)}</div>
+      <div className="info-item">Total distance: {formatDistance(pathDistance)}</div>
     </div>
   );
 }
